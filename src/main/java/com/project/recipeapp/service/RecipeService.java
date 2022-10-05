506eadbbe58f454aefa33896c0c5a86ff16f10a6
Kim-DaHam <times7749@naver.com>
2022-10-05 17:46:26 +0900

    create RecipeService.java

--- src/main/java/com/project/recipeapp/service/RecipeService.java
+++ src/main/java/com/project/recipeapp/service/RecipeService.java
@@ -1,8 +1,15 @@
 package com.project.recipeapp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.recipeapp.model.FridgeEntity;
+import com.project.recipeapp.model.MyRecipeEntity;
+import com.project.recipeapp.persistence.IngredientRepository;
+import com.project.recipeapp.persistence.MyIngredientRepository;
+import com.project.recipeapp.persistence.MyRecipeRepository;
 import com.project.recipeapp.persistence.RecipeRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +19,18 @@ import lombok.extern.slf4j.Slf4j;
 public class RecipeService {
 	
 	@Autowired
-	private RecipeRepository repository;
+	private RecipeRepository Rrepository;
+	@Autowired
+	private MyRecipeRepository MRrepository;
+	@Autowired
+	private IngredientRepository Irepository;
+	@Autowired
+	private MyIngredientRepository MIrepository;
 	
-	//public List<RecipeRepository> create 
+	public String create(final MyRecipeEntity entity){
+		String msg = "my recipe is updated.";
+		validate(entity);
+		MRrepository.save(entity);
+		return msg;
+	}
 }
